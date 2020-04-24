bcb3a19bb58896cf38dcfae030380b66d1f0cdfb
hjy9946 <hjy9946@naver.com>
2020-04-24 14:27:41 +0900

    Updates

---
+++ precache-manifest.502681d6ccd4708686c6ca1b5f594943.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "426bea311ccdf80078a56c56a00cf212",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "f9f2bccdb070f43abfc3",
+    "url": "/movie_app_2020/static/css/main.481b0001.chunk.css"
+  },
+  {
+    "revision": "08f6efee45481d1ea76e",
+    "url": "/movie_app_2020/static/js/2.19d622a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.19d622a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9f2bccdb070f43abfc3",
+    "url": "/movie_app_2020/static/js/main.969a003e.chunk.js"
+  },
+  {
+    "revision": "e76fc168c1d893da3cab",
+    "url": "/movie_app_2020/static/js/runtime-main.ea46b0e5.js"
+  }
+]);
